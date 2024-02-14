8b644da2a86ce53d35fd6c83718af75ece17ef84
Thomas <thomasjungcoe@gmail.com>
2024-02-14 00:46:50 -0600

    done with bubble sort

---
+++ BubbleSort/bubble_sort.js
@@ -0,0 +1,14 @@
+"use strict";
+exports.__esModule = true;
+function BubbleSort(arr) {
+    for (var i = 0; i < arr.length; i++) {
+        for (var j = 0; j < arr.length - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                var tmp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = tmp;
+            }
+        }
+    }
+}
+exports["default"] = BubbleSort;
